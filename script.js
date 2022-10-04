06bf0654668ee3a05ba8046dd616e7f3d34e9b10
Nícolas <nicolasbrga01@gmail.com>
2022-10-04 15:26:19 -0300

    requisito 1 feito

---
+++ script.js
@@ -0,0 +1,25 @@
+//Variáveis globais
+const body = document.body;
+
+//Requisito 1
+const h1 = document.createElement('h1');
+h1.id = 'title';
+h1.innerText = "Paleta de Cores";
+body.appendChild(h1);
+console.log(h1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Chamada de funções
